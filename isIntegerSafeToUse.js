c64eb770333d73703fe6002b5107e0699cd57f97
aframuz <aframuz@tutanota.io>
2021-04-26 23:29:38 -0400

    4/26/2021 challenges

--- isIntegerSafeToUse.js
+++ isIntegerSafeToUse.js
@@ -1,8 +1,13 @@
 function SafeInteger(n) {
    maxInteger = Math.pow(2,53)
-   if(n>-(maxInteger) || n<maxInteger){
+   if(n>-(maxInteger) && n<maxInteger){
       return true
    } else {
       return false
    }
 }
+
+//ANSWERS
+function SafeInteger(n) {
+   return Number.isSafeInteger(n);
+ }
