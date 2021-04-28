d51499eef8e4a10f72241e9d7eb8a80fd2a99b9b
aframuz <aframuz@tuta.io>
2021-04-28 10:33:29 -0400

    4/28/2021 challenges

---
+++ switchCase.js
@@ -0,0 +1,36 @@
+function evalObject(value){
+   var result = 0;
+   switch(value.operation){
+     case'+': 
+      result = value.a + value.b;
+      break
+     case'-': 
+      result = value.a - value.b;
+      break
+     case'/': 
+      result = value.a / value.b;
+      break
+     case'*': 
+      result = value.a * value.b;
+      break
+     case'%': 
+      result = value.a % value.b;
+      break
+     case'^': 
+      result = Math.pow(value.a, value.b);
+      break
+   }
+   return result;
+ }
+
+ //ANSWERS
+ function evalObject(value){
+   switch(value.operation){
+     case'+': return value.a + value.b;
+     case'-': return value.a - value.b;
+     case'/': return value.a / value.b;
+     case'*': return value.a * value.b;
+     case'%': return value.a % value.b;
+     case'^': return Math.pow(value.a, value.b);
+   }
+ }
