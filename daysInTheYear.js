891ff0f2c8f42d953451b2206b4e7a536e48d211
aframuz <aframuz@tuta.io>
2021-05-09 11:51:07 -0400

    5/9/2021 challenges

--- daysInTheYear.js
+++ daysInTheYear.js
@@ -7,3 +7,13 @@ function yearDays(year) {
       return `${year} has 365 days`
    }
 }
+
+// ANSWERS
+function yearDays(year) {
+   return (
+      year +
+      " has " +
+      ((!(year % 100) && year % 400) || year % 4 ? "365" : "366") +
+      " days"
+   )
+}
