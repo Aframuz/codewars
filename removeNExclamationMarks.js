929a44a61856f4436ff76958a3514fc8c759b925
aframuz <aframuz@tuta.io>
2021-06-01 20:34:50 -0400

    6/1/2021 challenges

--- removeNExclamationMarks.js
+++ removeNExclamationMarks.js
@@ -1,14 +1,6 @@
 function remove(s, n) {
-   if (n > s.length) {
-      return (s = s.replace(/!/g, ""))
-   }
    for (let i = 0; i < n; i++) {
-      if (s.indexOf("!") === 0) {
-         s = s.slice(1)
-      } else {
-         let index = s.indexOf("!")
-         s = s.slice(0, index) + s.slice(index + 1)
-      }
+      s = s.replace(/!/, "")
    }
    return s
 }
