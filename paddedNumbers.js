b9fa56baf501444cca07cf779ebaa43dcec65faa
aframuz <aframuz@tuta.io>
2021-06-12 12:39:19 -0400

    6/12/2021 challenges

---
+++ paddedNumbers.js
@@ -0,0 +1,4 @@
+function solution(value) {
+   value = String(value).padStart(5, "0")
+   return `Value is ${value}`
+}
---
+++ sumTheStrings.js
@@ -0,0 +1,8 @@
+function sumStr(a, b) {
+   return +a + +b + ""
+}
+
+// ANSWERS
+function sumStr(a, b) {
+   return String(Number(a) + Number(b))
+}
