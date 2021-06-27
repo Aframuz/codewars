f947d09a7f347c682fbb4f14f8739fe899bc8ff3
aframuz <aframuz@tuta.io>
2021-06-27 19:02:48 -0400

    6/27/2021 challenges

---
+++ coefficientsQuadratic.js
@@ -0,0 +1,4 @@
+function quadratic(x1, x2) {
+   // ax^2 +bx + c = 0
+   return [1, (x1 + x2) * -1, x1 * x2]
+}
---
+++ triangleArea.js
@@ -0,0 +1,4 @@
+function tArea(tStr) {
+   let arr = tStr.split(".")
+   arr.filter((s) => s === " ")
+}
