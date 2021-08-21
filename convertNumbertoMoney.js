4829456ddd0e06f8f4953ecc23d235470f328304
aframuz <aframuz@tuta.io>
2021-08-20 20:43:53 -0400

    8/20/2021 challenges

--- convertNumbertoMoney.js
+++ convertNumbertoMoney.js
@@ -10,3 +10,11 @@ var numberToMoney = function (n) {
       return nArr.join("") + "." + nFixed.split(".")[1]
    } else return nArr.join("")
 }
+
+// ANSWERS
+function numberToMoney(n) {
+   return String(Math.floor(n * 100) / 100).replace(
+      /\B(?=(\d{3})+(?!\d))/g,
+      ","
+   )
+}
